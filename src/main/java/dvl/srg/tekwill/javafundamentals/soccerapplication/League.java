74bdb2e3f0ea6b4d615f212b9b023fdd0d3749ac
Serghei Motpan <Serghei.Motpan@endava.com>
2017-08-11 09:16:30 +0300

    add scoregoal  method fir class Game

--- src/main/java/dvl/srg/tekwill/javafundamentals/soccerapplication/League.java
+++ src/main/java/dvl/srg/tekwill/javafundamentals/soccerapplication/League.java
@@ -28,21 +28,27 @@ public class League {
         Team realMadrid = new Team("Real Madrid", realMadridPlayers);
 
         // create a game
+        Game realVsBarcelona = new Game(realMadrid, barcelona);
+
         Goal goal1ScoredByLeoMessi = new Goal(barcelona, leoMessi, 25.30);
+        realVsBarcelona.scoreGoal(goal1ScoredByLeoMessi);
+
         Goal goal1ScoredByGarethBale = new Goal(realMadrid, garethBale, 30.00);
+        realVsBarcelona.scoreGoal(goal1ScoredByGarethBale);
+
         Goal goal2ScoredByLeoMessi = new Goal(barcelona, leoMessi, 50.30);
+        realVsBarcelona.scoreGoal(goal2ScoredByLeoMessi);
+
         Goal goal2ScoredByGarethBale = new Goal(realMadrid, garethBale, 62.44);
+        realVsBarcelona.scoreGoal(goal2ScoredByGarethBale);
+
+        Goal goalScoredByLuisSuarez = new Goal(barcelona, luisSuarez, 70.30);
+        realVsBarcelona.scoreGoal(goalScoredByLuisSuarez);
+
         Goal goalScoredBySergioRamos = new Goal(realMadrid, sergioRamos, 90.20);
+        realVsBarcelona.scoreGoal(goalScoredBySergioRamos);
 
-        Goal [] goals = new Goal[] { goal1ScoredByLeoMessi,
-                                     goal1ScoredByGarethBale,
-                                     goal2ScoredByLeoMessi,
-                                     goal2ScoredByGarethBale,
-                                     goalScoredBySergioRamos};
-
-        Game realVsBarcelona = new Game(realMadrid, barcelona, goals);
-
-        // create goals objects
+        // print info
         barcelona.printTeamPlayersNames();
         System.out.println();
         realMadrid.printTeamPlayersNames();
